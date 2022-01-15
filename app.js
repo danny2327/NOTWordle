afa4810584e57daab34e255eafab81bd8a186815
danny2327 <dstrood@gmail.com>
2022-01-15 00:50:42 -0800

    DId a whole bunch
    letters all work
    classes added and correctly displaying

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const letters = 5;
 let currentWord = 0;
 let currentChar = 0;
 
+
+
 const WORD = 'COUGH'
 
 function drawWords() {
@@ -28,38 +30,87 @@ function createEventListeners() {
   kb.addEventListener('click', (e) => {
     
     if(e.target.id === 'back') { 
-      console.log('deleted!');
+      deleteLastLetter();
     }
     else if(e.target.id === 'enter') { 
-      console.log('submitted!');
+      pressEnter();
     } else {
       // e.target.id
       if(!e.target.id.startsWith('row') && e.target.id !== 'keyboard') {
-        console.log(e.target.id);
         enterLetter(e.target.id);
       }
     }
   })
 }
 
+function deleteLastLetter() {
+  console.log(currentChar);  
+  const chars = wordsArray[currentWord].getElementsByClassName('letterbox')  
+  chars[currentChar].textContent = '';
+  if(currentChar > 0) {
+    currentChar--
+  }  
+}
 
-function enterLetter(letter) {
-  //if word not full
+
+function pressEnter() {
+  if(currentChar == letters-1) {
+    submitWord();
+  }
+}
+
+function submitWord() {
+   guess = readWord()
+   correct = showLetters(guess);
+   if(correct === letters) { 
+     win()
+   } else {
+     currentWord === words-1 ? gameOver() : nextWord();
+   }
+
+}
+
+function gameOver() {
+  console.log("Game Over");
+}
+
+function readWord() {
+  let guess = ''
   let word = wordsArray[currentWord];
   let chars = word.getElementsByClassName('letterbox')
-  let char = chars[currentChar]
-  char.textContent = letter
-  console.log(char);
-  console.log(chars[currentChar+1]);
-  
-  if(currentChar < letters-1) {
-    currentChar++;
+  for (let c = 0; c < chars.length; c++) {
+    const char = chars[c];
+    console.log(char, char.textContent);
+    guess += char.textContent;    
+  }  
+  return guess;
+}
+
+function showLetters(guess) {
+  let chars = wordsArray[currentWord].getElementsByClassName('letterbox')
+  let correct = 0;
+  for (let i = 0; i < letters; i++) {
+    console.log(WORD.indexOf(guess[i]));
+    if(WORD[i] === guess[i]) {
+      console.log("match!");
+      correct++;
+      chars[i].classList.add('rightplace')
+    } else if (WORD.indexOf(guess[i]) > -1) {
+      chars[i].classList.add('wrongplace')
+    }
+    return correct
   }
 }
 
 
-
-
+function enterLetter(letter) {
+  //if word not full
+  let chars = wordsArray[currentWord].getElementsByClassName('letterbox')
+  chars[currentChar].textContent = letter  
+  if(currentChar < letters-1) {
+    currentChar++;
+  }
+}
 
 function setup() {
   drawWords();
